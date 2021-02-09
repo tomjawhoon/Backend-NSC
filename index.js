d5230125830725147efd72e294773c0512a78706
tomjawhoon <naratorn_152@hotmail.com>
2021-02-09 23:46:07 +0700

    ed

--- index.js
+++ index.js
@@ -851,7 +851,7 @@ app.get('/test', (req, res) => {
     res.send("success good job !!!!!")
 })
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 6001;
 
 app.listen(PORT, () => {
     console.log(`Start server at port ${PORT}.`)
